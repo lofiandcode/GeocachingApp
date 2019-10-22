69af88149fbf0afb417ea47f265c4232028c3caa
github username <josephtrettevik@gmail.com>
2019-10-22 10:23:52 -0700

    switched DB to Postgres and seeded some user data

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
     has_many :usercaches
     has_many :caches, through: :usercaches
+end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+users = []
+5.times do
+    users.push(
+        User.create(
+            username: Faker::Name.name, 
+            password: Faker::Lorem.characters(number: 10), 
+            bio: Faker::Games::WorldOfWarcraft.quote
+        )
+    )
+end
+
+puts "Data Seeded."
+# caches = []
+# for(i=0;i<10;i++) {
+#     caches.push(Cache.create())
+# }
+
+
+
+
+
