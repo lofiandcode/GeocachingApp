38a5b3dfc8673302b81cd3a3fa41511add23995c
github username <github email address>
2019-10-23 13:30:21 -0700

    added: UserForm, UserSearchBar, CurrentLocation, Login folder,MapStyles(null)

--- frontend/src/containers/UserCachesContainer.js
+++ frontend/src/containers/UserCachesContainer.js
@@ -1,20 +1,51 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import UserCaches from '../components/UserCaches';
-import MoreButton from '../components/MoreButton'
+import MoreButton from '../components/MoreButton';
+import UserSearchBar from '../components/UserSearchBar'
+// import UserForm from '../components/UserForm';
 
+class UserCachesContainer extends Component {
+    constructor(){
+        super()
+        this.state = {
+          caches: [],
+          displayCaches: []
+        }
+      }
 
-const UserCachesContainer = (props) => {
-    return(
+    filterCaches = (type) => {
+        if(type !== "All"){
+          this.setState({
+            displayCaches: this.state.caches.filter(cache => cache.type === type)        
+          })
+        }else{
+          this.setState({
+            displayCaches: this.state.caches
+          })
+        }
+      };
+    
+    render() {
+        return(
         <Fragment>
+            {/* <UserForm />  */}
+                {/* fetchUsers={this.fetchUsers} /> */}
+            <br/>
             <div className="userCacheContainer">
-                {props.caches.map(cache => {
+                <UserSearchBar 
+                    filterCaches={this.filterCaches}
+                    type={this.props.type}
+                    rating={this.props.rating}
+                    difficulty={this.props.difficulty}
+                />
+                {this.props.caches.map(cache => {
                     return <UserCaches 
                                 key={cache.id}
                                 cache={cache} />
                 })}
-                <MoreButton fourMore={props.fourMore}/>
+                <MoreButton fourMore={this.props.fourMore}/>
             </div>
         </Fragment>
-    )
+    )}
 }
 export default UserCachesContainer;
