9886feef99591a1540a2ed994d6a100e5d6db9c1
github username <github email address>
2019-10-24 14:14:05 -0700

    Made route for new cache form

--- geo-app-backend/app/models/history.rb
+++ geo-app-backend/app/models/history.rb
@@ -1,4 +1,12 @@
 class History < ApplicationRecord
   belongs_to :user
   belongs_to :cache
+
+  def reviewer
+    return 0
+    # History.all.each do |history|
+    #   reviewer = history.id
+    # end
+  end
+
 end
--- geo-app-backend/app/serializers/cache_serializer.rb
+++ geo-app-backend/app/serializers/cache_serializer.rb
@@ -1,3 +1,3 @@
 class CacheSerializer < ActiveModel::Serializer
-  attributes :id, :name, :size,  :average_difficulty, :average_rating, :creator, :description, :mystery, :hint, :warnings, :coordinates, :photo, :histories
+  attributes :id, :name, :size,  :average_difficulty, :average_rating, :creator, :description, :mystery, :hint, :warnings, :coordinates, :photo, :reviewer, :histories, :users
 end
